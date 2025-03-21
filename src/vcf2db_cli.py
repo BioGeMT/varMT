c022859aab0c3f8637958426345b5c2f4f516bcc
Andrea Gariboldi <andrea.gariboldi@um.edu.mt>
2025-03-21 14:41:28 +0100

    Refactor argument parser setup in vcf2db_cli.py for improved readability and organization

--- src/vcf2db_cli.py
+++ src/vcf2db_cli.py
@@ -1,16 +1,19 @@
 from argparse import ArgumentParser
 
 def setup_args():
-    parser = \
-            ArgumentParser(prog="varMTdb",
-                           description="Tool for parsing VCF files and store data in PostreSQL"
-                           )
+    parser = ArgumentParser(
+        prog="varMTdb",
+        description="Tool for parsing VCF files and store data in PostreSQL"
+    )
 
+    # Database connection arguments
     parser.add_argument("-d", "--database", help="PostgreSQL DB name.", required=True)
-    parser.add_argument("-c", "--create", action="store_true", help="Delete and recreate DB if existing.", required=False)
     parser.add_argument("-u", "--username", help="PostgreSQL user." , required=True)
     parser.add_argument("-p", "--password", help="PostgreSQL user password.", required=True)
     parser.add_argument("-l", "--host", help="PostgreSQL host name", required=True)
+
+    # Database creation and manipulation arguments
+    parser.add_argument("-c", "--create", action="store_true", help="Delete and recreate DB if existing.", required=False)
     parser.add_argument("-t", "--tables", action="store_true", help="Create the tables in the database.", required=False)
     parser.add_argument("-i", "--insert", action="store_true", help="Insert the data inside the tables", required=False)
 
